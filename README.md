e914b445d8902471704af0649697cedf0243440f
Jon Senterfitt <thetwosents@gmail.com>
2022-02-14 21:24:54 -0600

    Update

---
+++ README.md
@@ -0,0 +1,4 @@
+# Welcome Adventurer
+
+![Which will you choose](https://user-images.githubusercontent.com/3770257/153788953-2fadcd32-3f24-4ab9-869c-714edeb1b306.png)
+
---
+++ pages/index.js
@@ -0,0 +1,205 @@
+import Head from 'next/head'
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import Web3 from 'web3';
+import getWeb3 from '../utils/getWeb3';
+import getBalance from '../utils/getBalance';
+
+const supportedTokens = [
+  {
+    name: 'HYPE',
+    symbol: 'HYPE',
+    decimals: 18,
+    address: '0x9a77eE1DACCc13674B047F967F863B5C8A9f6807',
+  }
+]
+
+const titles = {
+  margin: '0px 20px',
+  padding: '0px',
+  fontSize: '1.5em',
+}
+
+export default function Index() {
+  const [loading, setLoading] = useState(true);
+  const [selection, setSelection] = useState(false);
+  const [user, setUser] = useState(false);
+  const [accounts, setAccounts] = useState(null);
+  const [balance, setBalance] = useState(false);
+  const [hypeBalance, setHypeBalance] = useState(false);
+  const [giveBalance, setGiveBalance] = useState(false);
+  const [tokenAddresses, setTokenAddresses] = useState([]);
+  const redPill = () => {
+    setSelection(true);    
+    getWeb3(true)
+      .then((result) => {
+        getBalance(result, '0x9a77eE1DACCc13674B047F967F863B5C8A9f6807')
+          .then(balance => {
+            // Activates a user
+            setUser(true);
+            setBalance(balance);
+          });
+      });
+  };
+
+  const getAccounts = async (web3) => {
+    let accounts = web3.eth.getAccounts()
+        .then(async (addr) => {
+          setAccounts(addr);
+          let address_array = [];
+          for (let i = 0; i < addr.length; i++) {
+            let balance = await web3.eth.getBalance(addr[i]);
+            let tokenAddress = supportedTokens.map(token => {
+              return {
+                address: token.address,
+                balance: web3.utils.fromWei(balance, 'ether'),
+                symbol: token.symbol,
+                decimals: token.decimals,
+                name: token.name
+              }
+            });
+            address_array.push(tokenAddress);
+          }
+          setTokenAddresses(address_array);
+          // setUser(true);
+          setLoading(false);
+        });
+
+      return true;
+  }
+
+  useEffect(() => {
+    const checkConnection = async () => {
+
+      // Check if browser is running Metamask
+      let web3;
+      if (window.ethereum) {
+        web3 = new Web3(window.ethereum);
+      } else if (window.web3) {
+        web3 = new Web3(window.web3.currentProvider);
+      };
+      
+      // Check if User is already connected by retrieving the accounts
+      getAccounts(web3) 
+    };
+    checkConnection();
+  }, []);
+
+  if (loading) return 'Loading...';
+
+  return (
+
+    <>
+      <div>
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/orange_kid.ttf"
+            as="font"
+            crossOrigin=""
+          />
+        </Head>
+      </div>
+      <>
+        
+        <div
+          className={'matrix'}>
+            {
+              user &&
+              <>
+                <div style={{textAlign: 'center', marginBottom: 50}}>
+                  {/* Display all the accounts and their balances */}
+                  
+                  {
+                    accounts.map((account, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className={'account'}>
+                            
+                          <div
+                            className={'account-name'}>
+                            Vault Account: <br />{account}
+                          </div>
+                          <br />
+                          <div
+                            className={'account-balance'}>
+                              Balance: <br />
+                            {tokenAddresses[index].map((token, index) => {
+                              return (
+                                <div
+                                  key={index}
+                                  className={'token-balance'}>
+                                  {token.symbol}: {token.balance}
+                                </div>
+                              )
+                            })}
+                          </div>
+                        </div>
+                      )
+                    })
+                  }
+
+                </div>
+                {/* Check if the wallet is connected to the client */}
+                {/* {
+                  selection &&
+                  <div
+                    className={'selection'}>
+                    <div
+                      className={'selection-title'}>
+                      <h1>
+                        <span
+                          style={titles}>
+                          Select a token to send
+                        </span>
+                      </h1>
+                    </div>
+                    <div
+                      className={'selection-options'}>
+                      {
+                        tokenAddresses.map((account, index) => {
+                          return (
+                            <div
+                              key={index}
+                              className={'selection-option'}>
+                              <div
+                                className={'selection-option-name'}>
+                                {account[0].name}
+                              </div>
+                              <div
+                                className={'selection-option-balance'}>
+                                {account[0].symbol}: {account[0].balance}
+                              </div>
+                            </div>
+                          )
+                        })
+                      }
+                    </div>
+                  </div>
+                } */}
+                <Link href={'/gatekeeper'}><a className={'button'} href="">Enter Metaverse</a></Link>
+              </>
+            }
+
+            {
+              !user &&
+              <>
+            <h1>We've been waiting for you.</h1>
+            <div className={'row'}>
+              <div
+                className={'button'}
+                onClick={redPill}
+              > Red Pill Me </div>
+              <br />
+            </div>
+          </>
+            }
+          
+        </div>
+        
+        
+        </>
+      </>
+  );
+}
